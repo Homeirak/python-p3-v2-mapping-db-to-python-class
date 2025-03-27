658f1934bc907f815260ea81d15cf03e8949976f
homeira kalim <homeirakalim@gmail.com>
2025-03-27 15:09:18 -0400

    sql python mapping db to python class lab complete

--- lib/department.py
+++ lib/department.py
@@ -3,6 +3,8 @@ from __init__ import CURSOR, CONN
 
 class Department:
 
+    all = {}
+
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
@@ -45,6 +47,8 @@ class Department:
         CONN.commit()
 
         self.id = CURSOR.lastrowid
+        # stores the current object (self) in a class-level dictionary called all, using its id as the key
+        type(self).all[self.id] = self
 
     @classmethod
     def create(cls, name, location):
@@ -69,6 +73,70 @@ class Department:
             DELETE FROM departments
             WHERE id = ?
         """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+
+        # Delete the dictionary entry using id as the key
+        del type(self).all[self.id]
+
+        # Set the id to None
+        self.id = None
+        
+    @classmethod
+    # method that converts the row from the db into Department object (Department instance), using caching to avoid duplicates.
+    def instance_from_db(cls, row):
+        """Return a Department object having the attribute values from the table row."""
+
+        # Checks if an instance with id = row[0] already exists in the class-level dictionary cls.all. If it exists, it's returned. If not, None is returned.
+        department = cls.all.get(row[0])
+        # If the department instance with that id already exists in the cls.all dictionary, we reuse it instead of creating a new one.
+        if department:
+            # ensure attributes match row values in case local object was modified;  if something changed in the database, you want your object in Python to reflect that updated data.
+            department.name = row[1]
+            department.location = row[2]
+        else:
+            # not in dictionary, create new instance and add to dictionary
+            # Creates a new instance using the name and location from the row.
+            department = cls(row[1], row[2])
+            department.id = row[0]
+            cls.all[department.id] = department
+        return department
+    
+    @classmethod
+    def get_all(cls):
+        """Return a list containing a Department object per row in the table"""
+        sql = """
+            SELECT *
+            FROM departments
+        """
+
+        rows = CURSOR.execute(sql).fetchall()
+
+        return [cls.instance_from_db(row) for row in rows]
+    
+    @classmethod
+    def find_by_id(cls, id):
+        """Return a Department object corresponding to the table row matching the specified primary key"""
+        sql = """
+            SELECT *
+            FROM departments
+            WHERE id = ?
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
+    @classmethod
+    def find_by_name(cls, name):
+        """Return a Department object corresponding to first table row matching specified name"""
+        sql = """
+            SELECT *
+            FROM departments
+            WHERE name is ?
+        """
+
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
 
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
